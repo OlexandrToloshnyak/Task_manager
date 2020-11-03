cc607dadebaa1da86cc11c04d2fa85a83cdfec7b
Oleksandr Toloshnyak <toloshnyakprogram@gmail.com>
2020-11-03 22:56:07 +0200

    bug fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,6 +57,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { :host => "rubyrailstaskmanager.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
