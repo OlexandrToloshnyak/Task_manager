8ec69ebd76d744a2bad5e051b6e05c94cff73dfa
Oleksandr Toloshnyak <toloshnyakprogram@gmail.com>
2020-11-03 22:18:46 +0200

    add authentication

--- Gemfile
+++ Gemfile
@@ -7,7 +7,11 @@ ruby '2.7.2'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3', '~> 1.4'
+# Use devise as authefication manager
+gem 'devise'
+# Use postgresql as the database for Active Record
 gem 'postgresql'
+# Use simple form to easy generate html forms
 gem 'simple_form'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,9 +53,23 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'smtp.gmail.com',
+  :user_name => "railstaskmanager@gmail.com",
+  :password => "1QAZ1QAZ",
+  :authentication => :login,
+  :enable_starttls_auto => true
+} 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
